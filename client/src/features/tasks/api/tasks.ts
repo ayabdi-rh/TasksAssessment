30ff3c2a5feb0f1bc73811073fc7a73d1c8ff8ac
ayabdi <46926470+ayabdi@users.noreply.github.com>
2024-08-20 17:06:08 +0300

    fix task errors

--- client/src/features/tasks/api/tasks.ts
+++ client/src/features/tasks/api/tasks.ts
@@ -33,7 +33,7 @@ export const useCreateTask = () => {
       queryClient.invalidateQueries({ queryKey: ['tasks'] })
     },
     onError: (error: any) => {
-      toast.error(error.response.data.error)
+      toast.error(error.response.data)
     }
   })
 }
@@ -54,7 +54,7 @@ export const useUpdateTask = () => {
       queryClient.invalidateQueries({ queryKey: ['tasks'] })
     },
     onError: (error: any) => {
-      toast.error(error.response.data.error)
+      toast.error(error.response.data)
     }
   })
 }
@@ -75,7 +75,7 @@ export const useDeleteTask = () => {
       queryClient.invalidateQueries({ queryKey: ['tasks'] })
     },
     onError: (error: any) => {
-      toast.error(error.response.data.error)
+      toast.error(error.response.data)
     }
   })
 }
