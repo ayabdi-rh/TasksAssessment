024a9a8dc481bc95e3d629841a2a5fb820691a10
ayabdi <46926470+ayabdi@users.noreply.github.com>
2024-08-21 10:14:56 +0300

    change error response to string

--- server/app/routes/tasks.ts
+++ server/app/routes/tasks.ts
@@ -10,7 +10,7 @@ router.get("/", async (req: Request, res: Response) => {
     const tasks = await getTasks(req.user!.id);
     res.json(tasks);
   } catch (error: any) {
-    res.status(500).json({ message: error.message });
+    res.status(500).send(error.message);
   }
 });
 
@@ -22,12 +22,15 @@ router.post("/", async (req: Request, res: Response) => {
 
     // Validate input
     if (!name) {
-      return res.status(400).json({ message: "Task Name is required" });
+      return res.status(400).send("Task Name is required");
     }
 
     // Validate Status
     if (status && !validStatus.includes(status)) {
-      return res.status(400).json({ message: "Invalid status" });
+      return res.status(400).json({
+        message:
+          "Invalid status. Valid Statuses: 'BACKLOG', 'TODO', 'IN_PROGRESS', 'COMPLETE'",
+      });
     }
 
     // Create new task
@@ -40,7 +43,7 @@ router.post("/", async (req: Request, res: Response) => {
 
     res.status(201).json(newTask);
   } catch (error: any) {
-    res.status(500).json({ message: error.message });
+    res.status(500).send(error.message);
   }
 });
 
@@ -50,14 +53,14 @@ router.patch("/:id", async (req: Request, res: Response) => {
 
     // Validate input
     if (Object.keys(req.body).length === 0) {
-      return res.status(400).json({ message: "No update data provided" });
+      return res.status(400).send("No update data provided");
     }
 
     // Validate status is correct
     const { status } = req.body;
 
     if (status && !validStatus.includes(status)) {
-      return res.status(400).json({ message: "Invalid status" });
+      return res.status(400).send("Invalid status");
     }
 
     // Update task
@@ -71,7 +74,7 @@ router.patch("/:id", async (req: Request, res: Response) => {
 
     res.json(updatedTask);
   } catch (error: any) {
-    res.status(500).json({ message: error.message });
+    res.status(500).send(error.message);
   }
 });
 
@@ -83,12 +86,12 @@ router.delete("/:id", async (req: Request, res: Response) => {
     const deletedTask = await deleteTask(id);
 
     if (!deletedTask) {
-      return res.status(404).json({ message: "Task not found" });
+      return res.status(404).send("Task not found");
     }
 
-    res.json({ message: "Task deleted successfully" });
+    res.send("Task deleted successfully");
   } catch (error: any) {
-    res.status(500).json({ message: error.message });
+    res.status(500).send(error.message);
   }
 });
 export default router;
